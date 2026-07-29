be7a6163d05c5fc6dd2877a399001d8e27dfb977
pnchsb <a123456asdhh@qq.com>
2026-07-29 23:43:59 +0800

    [feat] 完成fursuist_test的基本模型注册，完成FursuitKey热键的注册

---
+++ src/main/java/top/fur/furrybohe/register/RegisterCommand.java
@@ -0,0 +1,16 @@
+package top.fur.furrybohe.register;
+
+import net.minecraftforge.event.RegisterCommandsEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import top.fur.furrybohe.command.tests.AlwaysTPCommand;
+import top.fur.furrybohe.config.repo_configs.ModInfo;
+
+@Mod.EventBusSubscriber(modid = ModInfo.MODID, bus = Mod.EventBusSubscriber.Bus.FORGE)
+public class RegisterCommand {
+
+    @SubscribeEvent
+    public static void onRegisterCommands(RegisterCommandsEvent event) {
+        AlwaysTPCommand.register(event.getDispatcher());
+    }
+}
